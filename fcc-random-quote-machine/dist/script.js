c1e17b8efbd1caa618efc6354e48d27b3aa528da
Jamila Toaha <jtoahacoding@gmail.com>
2020-05-31 01:35:22 -0400

    linked state to a random quote, added button to display random quote

--- fcc-random-quote-machine/dist/script.js
+++ fcc-random-quote-machine/dist/script.js
@@ -1,44 +1,29 @@
-// !! IMPORTANT README:
 
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place.
-
-/***********
-INSTRUCTIONS:
-  - Select the project you would
-    like to complete from the dropdown
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank
-    pen.
-  - Click the "TESTS" button to see
-    the individual test cases.
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go
-    from red to green.
-  - As you start to build out your
-    project, when tests are failing,
-    you should get helpful errors
-    along the way!
-    ************/
-
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
-
-// Once you have read the above messages, you can delete all comments.
-
-/*Example of a class component and rendering it */
 class QuoteBox extends React.Component {
   constructor(props){
     super(props)
     this.randomQuoteIndex = Math.floor(Math.random()*20)
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      currentQuote: this.props.quotes[this.randomQuoteIndex]
+    }
   }
+
+  handleClick(){
+    this.randomQuoteIndex = Math.floor(Math.random()*20);
+    this.setState({
+      currentQuote: this.props.quotes[this.randomQuoteIndex]
+    })
+  }
+
   render() {
-    let currentQuote = this.props.quotes[this.randomQuoteIndex];
+    let currentQuote = this.state.currentQuote;
     return (
       <div>
-        <h1>My First React Component!</h1>
-    <h3>{currentQuote.text}</h3>
-    <h4>~{currentQuote.author}</h4>
+        <h1>Quote Machine</h1>
+    <h3 id="text">{currentQuote.text}</h3>
+    <h4 id="author">–{currentQuote.author}</h4>
+    <button type="submit" onClick={this.handleClick} id="new-quote"> New Quote </button>
       </div>
     );
   }
