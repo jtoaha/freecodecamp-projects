eee117db8b414e627308b20dccb7eb6e01603836
Jamila Toaha <jtoahacoding@gmail.com>
2020-05-31 02:41:16 -0400

    added twitter library and added tweet button so quote can be shared

--- fcc-random-quote-machine/dist/script.js
+++ fcc-random-quote-machine/dist/script.js
@@ -16,6 +16,10 @@ class QuoteBox extends React.Component {
     })
   }
 
+  // componentDidMount() {
+  //   window.twttr.widgets.load()
+  // }
+
   render() {
     let currentQuote = this.state.currentQuote;
     return (
@@ -24,6 +28,9 @@ class QuoteBox extends React.Component {
     <h3 id="text">{currentQuote.text}</h3>
     <h4 id="author">–{currentQuote.author}</h4>
     <button type="submit" onClick={this.handleClick} id="new-quote"> New Quote </button>
+
+
+    <a href="https://twitter.com/intent/tweet" className="twitter-share-button" data-text={`"${currentQuote.text}" –${currentQuote.author}`} data-show-count="false" data-size="large" data-url=" "></a>
       </div>
     );
   }
@@ -33,8 +40,8 @@ class QuoteBox extends React.Component {
 
 let quotes = [
   {text: 'Start where you are. Use what you have. Do what you can.', author: 'Arthur Ashe'},
-  {text: 'I hated every minute of training, but I said, "Don\'t quit. Suffer now and live the rest of your life as a champion.', author: 'Muhammad Ali'},
-  {text: 'The secret of change is to focus all your energy not on fighting the old but on building the new', author: 'David Millman'},
+  {text: 'I hated every minute of training, but I said, \'Don\'t quit. Suffer now and live the rest of your life as a champion.\'', author: 'Muhammad Ali'},
+  {text: 'The secret of change is to focus all your energy not on fighting the old but on building the new.', author: 'David Millman'},
   {text: 'Go confidently in the direction of your dreams. Live the life you have imagined.', author: 'Henry David Thoreau'},
   {text: 'May you live every day of your life.', author: 'Jonathan Swift'},
   {text: 'Success isn\'t owned. It is leased, and rent is due.', author: 'JJ Watts'},
@@ -45,7 +52,7 @@ let quotes = [
   {text: 'Remember, you alone get to choose what matters and what doesn’t. The meaning of everything in your life has precisely the meaning you give it.', author: 'Marc and Angel'},
   {text: 'Let go of the thing that you’re trying to be (the noun), and focus on the actual work you need to be doing (the verb).', author: 'Austin Kleon'},
   {text: 'Life isn’t about finding yourself. Life is about creating yourself.', author: 'George Bernard Shaw'},
-  {text: 'Richness does not lie in the abundance of (worldly) goods but richness is the richness of the soul (heart, self)', author: 'Prophet Muhammed'},
+  {text: 'Richness does not lie in the abundance of (worldly) goods but richness is the richness of the soul (heart, self).', author: 'Prophet Muhammed'},
   {text: 'Opportunity does not knock, it presents itself when you beat down the door.', author: 'Kyle Chandler'},
   {text: 'There is only one corner of the universe you can be certain of improving, and that\'s your own self.', author: 'Aldous Huxley'},
   {text: 'You don\'t have to be great to start, but you have to start to be great.', author: 'Zig Ziglar'},
