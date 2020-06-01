d70879e4b3a43d3356ca3acd4d42f750da6ba5a0
Jamila Toaha <jtoahacoding@gmail.com>
2020-06-01 05:24:56 -0400

    set up Editor and Preview React Components and CSS layout

--- fcc-markdown-previewer/dist/script.js
+++ fcc-markdown-previewer/dist/script.js
@@ -1,1 +1,55 @@
 var testing;
+
+/*This Markdown Previewer is made using React, Bootstrap, and custom CSS. CodePen: https://codepen.io/jtoaha/pen/XWmvpVy?editors=0110*/
+
+class Editor extends React.Component {
+  constructor(props) {
+    super(props)
+
+    }
+
+
+  handleClick() {
+  }
+
+  render() {
+    return (
+      <div id= "editor-container">
+      <h2>Markdown Editor</h2>
+      <textarea id="editor-textarea"></textarea>
+      </div>
+    )
+  }
+}
+
+
+
+ReactDOM.render(
+  <Editor />,
+  document.getElementById('editor-section')
+)
+
+class Preview extends React.Component {
+  constructor(props) {
+    super(props)
+
+    }
+
+
+  handleClick() {
+  }
+
+  render() {
+    return (
+      <div id= "editor-container">
+      <h2>Markdown Preview</h2>
+
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Preview />,
+  document.getElementById('preview-section')
+)
