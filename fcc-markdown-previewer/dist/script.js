ef832b45b1039aba04e2ba8f66f647311aec3673
Jamila Toaha <jtoahacoding@gmail.com>
2020-06-02 05:04:41 -0400

    added readme

--- fcc-markdown-previewer/dist/script.js
+++ fcc-markdown-previewer/dist/script.js
@@ -1,49 +1,48 @@
-/*This Markdown Previewer is made using React, Redux, Bootstrap, and custom CSS. This being a Codepen project. All componets had to be named in one js file. CodePen: https://codepen.io/jtoaha/pen/XWmvpVy?editors=0110*/
+/*This Markdown Previewer is made using React, React-Redux, and vanilla CSS. This being a Codepen project. All componets had to be named in one js file. CodePen: https://codepen.io/jtoaha/pen/zYrOGqK/
+
+//Set parsing settings for marked library
 marked.setOptions({
   breaks: true,
   sanitize: false,
   smartLists: true,
   smartypants: false,
-  xhtml: false
-});
+  xhtml: false,
+})
 
-
-//Redux
 // Redux:
-const ADD = 'ADD';
+const ADD = 'ADD'
 
 const addMarkdown = (parsed) => {
   return {
     type: ADD,
-    parsed: parsed
+    parsed: parsed,
   }
-};
+}
 
 const initialState = {
-  parsed: ""
+  parsed: '',
 }
 const markdownReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD:
-      return {...state, parsed: action.parsed};
+      return { ...state, parsed: action.parsed }
     default:
-      return state;
+      return state
   }
-};
+}
 
-const store = Redux.createStore(markdownReducer);
+const store = Redux.createStore(markdownReducer)
 
 // React:
-const Provider = ReactRedux.Provider;
-const connect = ReactRedux.connect;
+const Provider = ReactRedux.Provider
+const connect = ReactRedux.connect
 
 class Editor extends React.Component {
   constructor(props) {
     super(props)
     this.handleChange = this.handleChange.bind(this)
     this.state = {
-      defaultTextArea:
-`Write some markdown style text and see it be rendered in the Preview Section.
+      defaultTextArea: `Write some markdown style text and see it be rendered in the Preview Section.
 # This is Heading 1 text
 ## This is Heading 2 text
 - First item
@@ -58,21 +57,20 @@ class Editor extends React.Component {
 \`\`\`
 print("Hello World!")
 \`\`\`
-> "Start where you are. Use what you have. Do what you can." –Arthur Ashe `
+> "Start where you are. Use what you have. Do what you can." –Arthur Ashe `,
     }
-    let parsed = marked(this.state.defaultTextArea);
-    this.props.addParsedMessage(parsed);
-
+    let parsed = marked(this.state.defaultTextArea)
+    this.props.addParsedMessage(parsed)
   }
 
   handleChange(event) {
-    let parsed = marked(event.target.value);
-    this.props.addParsedMessage(parsed);
+    let parsed = marked(event.target.value)
+    this.props.addParsedMessage(parsed)
   }
 
   //Allows autofocus of textarea element
   componentDidUpdate(prevProps, prevState) {
-    this._input.focus();
+    this._input.focus()
   }
 
   render() {
@@ -80,16 +78,14 @@ print("Hello World!")
       <div id='editor-section'>
         <div id='editor-container'>
           <h2>Markdown Editor</h2>
-          <hr/>
+          <hr />
           <textarea
-            ref={c => (this._input = c)}
+            ref={(c) => (this._input = c)}
             onChange={this.handleChange}
             placeholder='Write some markdown style text'
             id='editor'
-            defaultValue=
-             {this.state.defaultTextArea} >
-
-          </textarea>
+            defaultValue={this.state.defaultTextArea}
+          ></textarea>
         </div>
       </div>
     )
@@ -101,57 +97,48 @@ class Preview extends React.Component {
     super(props)
   }
 
-  handleClick() {
-
-  }
+  handleClick() {}
 
   render() {
     return (
       <div id='preview-section'>
         <div id='preview-container'>
           <h2>Markdown Preview</h2>
-          <hr/>
-          <div id='preview'
-          dangerouslySetInnerHTML={{ __html: this.props.parsed }}
-          >
-
-          </div>
+          <hr />
+          <div
+            id='preview'
+            dangerouslySetInnerHTML={{ __html: this.props.parsed }}
+          ></div>
         </div>
       </div>
     )
   }
 }
 
-
-
-
 const mapStateToProps = (state) => {
-  return {parsed: state.parsed}
-};
+  return { parsed: state.parsed }
+}
 
 const mapDispatchToProps = (dispatch) => {
   return {
     addParsedMessage: (parsed) => {
       dispatch(addMarkdown(parsed))
-    }
+    },
   }
-};
+}
 
-const Container1 = connect(mapStateToProps, mapDispatchToProps)(Editor);
-const Container2 = connect(mapStateToProps, mapDispatchToProps)(Preview);
-
-
+const Container1 = connect(mapStateToProps, mapDispatchToProps)(Editor)
+const Container2 = connect(mapStateToProps, mapDispatchToProps)(Preview)
 
 class AppWrapper extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Container1/>
-        <Container2/>
+        <Container1 />
+        <Container2 />
       </Provider>
-    );
+    )
   }
-};
+}
 
 ReactDOM.render(<AppWrapper />, document.getElementById('whole'))
-
