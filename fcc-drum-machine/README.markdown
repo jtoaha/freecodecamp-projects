d4f752d0706fa176fd4671a291083add44192f2c
Jamila Toaha <jtoahacoding@gmail.com>
2020-06-04 00:31:36 -0400

    added custom CSS and readme

--- fcc-drum-machine/README.markdown
+++ fcc-drum-machine/README.markdown
@@ -3,3 +3,14 @@
 A Pen created on CodePen.io. Original URL: [https://codepen.io/jtoaha/pen/Vwewpbv](https://codepen.io/jtoaha/pen/Vwewpbv).
 
 
+This Drum Machine is built as part of the Free Code Camp Front End Libraries Track. It is implemented by using React.js, vanilla CSS, and a bit of JQuery(primarily to add event handlers and CSS for mouse and keyboard events). Drum audio is accessed and streamed via DropBox, where I stored MP3 files.
+
+Overview: This Drum Machine project sets up a drum machine user can interact with, either by:
+- Pressing corresponding letter on their keyboard (i.e, Q, W, E, A, S, D, Z, X, C)
+- Or simply pressing the button
+Every button corresponds to a different sound effect.
+Notable CSS Effects added: When user presses keyboard or clicks on button, CSS shows feedback by showing the button being pressed in and briefly changing it's background color. Flexbox added so buttons space out evenly and is responsive across different screen sizes.
+
+Original Sound Effects Source: [http://www.flashkit.com/soundfx](http://www.flashkit.com/soundfx) This is a great website for sound effects. However, once Flash totally becomes obsolete by the end of 2020 may become more diffcult to navigate.
+
+-jt 06/04/20
--- fcc-drum-machine/dist/script.js
+++ fcc-drum-machine/dist/script.js
@@ -1,5 +1,10 @@
 /**
- *
+Overview: This Drum Machine project sets up a drum machine user can interact with, either by:
+- Pressing corresponding letter on their keyboard (i.e, Q, W, E, A, S, D, Z, X, C)
+- Or simply pressing the button
+Check the readme for this project for more helpful details.
+-jt
+06/04/20
  */
 const soundFiles = [
   {
@@ -32,42 +37,54 @@ const soundFiles = [
   },
   {
     id: 'S',
-    keycodeValue: '83',
+    keycodeValue: 83,
     url:
       'https://www.dropbox.com/s/58qp6j5r1okekoo/idg_HipH-intermed-1982_hifi.mp3?raw=1',
     description: 'IDG HipH Intermed 1982 hifi',
   },
   {
     id: 'D',
-    keycodeValue: '68',
+    keycodeValue: 68,
     url:
       'https://www.dropbox.com/s/3t5ryrkt5dq26gb/idg_HipH-intermed-2220_hifi.mp3?raw=1',
     description: 'IDG HipH Intermed 2220 hifi',
   },
   {
     id: 'Z',
-    keycodeValue: '90',
+    keycodeValue: 90,
     url:
       'https://www.dropbox.com/s/i9smjs55r3cju6x/idg_HipH-intermed-2222_hifi.mp3?raw=1',
     description: 'IDG HipH Intermed 2222 hifi',
   },
   {
     id: 'X',
-    keycodeValue: '88',
+    keycodeValue: 88,
     url:
       'https://www.dropbox.com/s/2b6ffkgrih966kc/idg_Hi_H-intermed-2300_hifi.mp3?raw=1',
     description: 'IDG Hi H Intermed 2300 hifi',
   },
   {
     id: 'C',
-    keycodeValue: '67',
+    keycodeValue: 67,
     url:
       'https://www.dropbox.com/s/yok22zzavl1et4u/idg-pian-intermed-1321_hifi.mp3?raw=1',
     description: 'IDG Pian Intermed 1321 hifi',
   },
 ]
 
-var currentSound = ''
+var randomColors = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'indigo',
+  'violet',
+  'magenta',
+  'cyan',
+]
+
+var randomColorIndex = 0
 
 class DrumPad extends React.Component {
   constructor(props) {
@@ -78,27 +95,79 @@ class DrumPad extends React.Component {
   handleChange(event) {}
 
   componentDidMount(prevProps, prevState) {
-    $(`#sound${this.props.soundFile.id}`).on('click', () => {
+    $(`#sound${this.props.soundFile.id}`).on('mousedown', () => {
+      $(`#sound${this.props.soundFile.id}`).css({
+        'background-color': 'green',
+        'box-shadow': '1px 5px #000',
+        transform: 'translateY(4px)',
+      })
+
       document.getElementById(this.props.soundFile.id).play()
-      currentSound = this.props.soundFile.description
+      let currentSound = this.props.soundFile.description
       this.props.updateCurrentSound(currentSound)
     })
 
+    $(`#sound${this.props.soundFile.id}`).on('mouseup', () => {
+      $(`#sound${this.props.soundFile.id}`).css({
+        'background-color': '#ff63b9',
+        padding: '1em',
+        'padding-right': '1.25em',
+        'padding-left': '1.25em',
+        margin: '.5em',
+        'border-radius': '50px',
+        'box-shadow': '5px 6px',
+      })
+    })
+
+    $(`#sound${this.props.soundFile.id}`).on('mouseout', () => {
+      $(`#sound${this.props.soundFile.id}`).css({
+        'background-color': '#ff63b9',
+        padding: '1em',
+        'padding-right': '1.25em',
+        'padding-left': '1.25em',
+        margin: '.5em',
+        'border-radius': '50px',
+        'box-shadow': '5px 6px',
+      })
+    })
+
     $(`body`).keydown((event) => {
-      var keycode = event.keyCode
-      console.log(keycode)
-      console.log(event.which)
+      var keycode = event.which
       if (keycode == this.props.soundFile.keycodeValue) {
         document.getElementById(this.props.soundFile.id).play()
-        currentSound = this.props.soundFile.description
+        $(`#sound${this.props.soundFile.id}`).css({
+          'background-color': 'green',
+          'box-shadow': '1px 5px #000',
+          transform: 'translateY(4px)',
+        })
+
+        let currentSound = this.props.soundFile.description
         this.props.updateCurrentSound(currentSound)
       }
+    })
+
+    $(`body`).keyup((event) => {
+      var keycode = event.which
+
+      $(`#sound${this.props.soundFile.id}`).css({
+        'background-color': '#ff63b9',
+        padding: '1em',
+        'padding-right': '1.25em',
+        'padding-left': '1.25em',
+        margin: '.5em',
+        'border-radius': '50px',
+        'box-shadow': '5px 6px',
+      })
     })
   }
 
   render() {
     return (
-      <button className='drum-pad' id={`sound${this.props.soundFile.id}`}>
+      <button
+        ref={(c) => (this._drumbutton = c)}
+        className={`drum-pad`}
+        id={`sound${this.props.soundFile.id}`}
+      >
         {this.props.soundFile.id}
         <audio
           id={this.props.soundFile.id}
@@ -113,7 +182,7 @@ class DrumPad extends React.Component {
 
 //To display description of the most recent sound byte that was clicked
 const Display = (props) => {
-  return <h2 id='display'>Display: {currentSound}</h2>
+  return <h2 id='display'>Display: {props.currentSound}</h2>
 }
 
 class DrumMachine extends React.Component {
@@ -122,7 +191,7 @@ class DrumMachine extends React.Component {
     this.handleChange = this.handleChange.bind(this)
     this.updateCurrentSound = this.updateCurrentSound.bind(this)
     this.state = {
-      currentSound: '',
+      currentSound: ' ',
     }
   }
   updateCurrentSound(val) {
