effc791f876e7551da11626ed0e4a1ae6a22edd6
Jamila Toaha <jtoahacoding@gmail.com>
2020-06-06 04:04:03 -0400

    adjusted css

--- fccjs-calculator/dist/script.js
+++ fccjs-calculator/dist/script.js
@@ -1,21 +1,17 @@
 //To display description of the most recent sound byte that was clicked
 const Display = (props) => {
-  return <h2 id='display'>Display: {props.displayValue
-  }</h2>
+  return <h2 id='display'>Display: {props.displayValue}</h2>
 }
 
 class CalcButton extends React.Component {
   constructor(props) {
     super(props)
     this.handleChange = this.handleChange.bind(this)
-
   }
 
   handleChange(event) {}
 
-  componentDidMount(prevProps, prevState) {
-
-  }
+  componentDidMount(prevProps, prevState) {}
 
   render() {
     return (
@@ -40,24 +36,23 @@ class JSCalculator extends React.Component {
     }
 
     this.buildNumAndOpsArray = [
-      {id: 'zero', keyVal: 0, type: 'number', func: ''},
-      {id: 'one', keyVal: 1, type: 'number', func: ''},
-      {id: 'two', keyVal: 2, type: 'number', func: ''},
-      {id: 'three', keyVal: 3, type: 'number', func: ''},
-      {id: 'four', keyVal: 4, type: 'number', func: ''},
-      {id: 'five', keyVal: 5, type: 'number', func: ''},
-      {id: 'six', keyVal: 6, type: 'number', func: ''},
-      {id: 'seven', keyVal: 7, type: 'number', func: ''},
-      {id: 'eight', keyVal: 8, type: 'number', func: ''},
-      {id: 'nine', keyVal: 9, type: 'number', func: ''},
-      {id: 'decimal', keyVal: '.', type: 'number', func: ''},
-      {id: 'clear', keyVal: 'AC', type: 'operator', func: ''},
-      {id: 'divide', keyVal: '/', type: 'operator', func: ''},
-      {id: 'multiply', keyVal: 'x', type: 'operator', func: ''},
-      {id: 'subtract', keyVal: '-', type: 'operator', func: ''},
-      {id: 'add', keyVal: '+', type: 'operator', func: ''},
-      {id: 'equals', keyVal: '=', type: 'operator', func: ''
-    }
+      { id: 'zero', keyVal: 0, type: 'number', func: '' },
+      { id: 'one', keyVal: 1, type: 'number', func: '' },
+      { id: 'two', keyVal: 2, type: 'number', func: '' },
+      { id: 'three', keyVal: 3, type: 'number', func: '' },
+      { id: 'four', keyVal: 4, type: 'number', func: '' },
+      { id: 'five', keyVal: 5, type: 'number', func: '' },
+      { id: 'six', keyVal: 6, type: 'number', func: '' },
+      { id: 'seven', keyVal: 7, type: 'number', func: '' },
+      { id: 'eight', keyVal: 8, type: 'number', func: '' },
+      { id: 'nine', keyVal: 9, type: 'number', func: '' },
+      { id: 'decimal', keyVal: '.', type: 'number', func: '' },
+      { id: 'clear', keyVal: 'AC', type: 'operator', func: '' },
+      { id: 'divide', keyVal: '/', type: 'operator', func: '' },
+      { id: 'multiply', keyVal: 'x', type: 'operator', func: '' },
+      { id: 'subtract', keyVal: '-', type: 'operator', func: '' },
+      { id: 'add', keyVal: '+', type: 'operator', func: '' },
+      { id: 'equals', keyVal: '=', type: 'operator', func: '' },
     ]
   }
   updateCurrentSound(val) {
@@ -73,21 +68,15 @@ class JSCalculator extends React.Component {
     return (
       <span>
         {/* <h1 id='main-title'>Javascript Calculator</h1> */}
-        <span id='grid-container'>
-        <Display displayValue={this.state.displayValue} />
-        {this.buildNumAndOpsArray.map((number) => (
-          <CalcButton
-            number={number}
-            key={number.id}
-          />
-        ))}
-        </span>
+        <div id='grid-container'>
+          <Display displayValue={this.state.displayValue} />
+          {this.buildNumAndOpsArray.map((number) => (
+            <CalcButton number={number} key={number.id} />
+          ))}
+        </div>
       </span>
     )
   }
 }
 
-ReactDOM.render(
-  <JSCalculator />,
-  document.getElementById('js-calculator')
-)
+ReactDOM.render(<JSCalculator />, document.getElementById('js-calculator'))
