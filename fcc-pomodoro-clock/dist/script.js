00e8dbbaad4657a9904cae22f0ee72434f3e2a59
Jamila Toaha <jtoahacoding@gmail.com>
2020-08-09 19:26:11 -0400

    added starter files for Pomodoro Clock project

---
+++ fcc-pomodoro-clock/dist/script.js
@@ -0,0 +1,7 @@
+class Pomodoro extends React.Component {
+  constructor(props) {
+    super(props);
+  }}
+
+
+ReactDOM.render(React.createElement(Pomodoro, null), document.getElementById('pomodoro-env'));
