c2246f519f4e8c532dcb7d2c61741971dce5f3fd
Jamila Toaha <jtoahacoding@gmail.com>
2020-08-09 20:18:18 -0400

    added CSS Grid layout and basic background items

--- fcc-pomodoro-clock/dist/script.js
+++ fcc-pomodoro-clock/dist/script.js
@@ -1,7 +1,46 @@
+
+//Background Items Array will be used to populate BackgroundItems components
+let backgroundItemsArray = [
+  {name: 'title-item', text: 'Pomodoro Clock built with React.js', color: '#ffffff'},
+  {name: 'sun-item', text: '', color: 'yellow'},
+  {name: 'horizontal-item', text: '', color: 'pink'},
+  {name: 'timer-case-item', text: '', color: 'red'},
+]
+
+
+const BackgroundItem = props => {
+  return (
+    <div id={props.name} style={{backgroundColor: props.color}}>
+        <h1>
+          {props.text}
+        </h1>
+    </div>
+  )
+}
+
+
+
 class Pomodoro extends React.Component {
   constructor(props) {
     super(props);
-  }}
+  }
 
+  render () {
+    return (
+      <div id="pomodoro">
+     {backgroundItemsArray.map(item =>
+      (  <BackgroundItem
+          key= {item.name}
+          name = {item.name}
+          text= {item.text}
+          color= {item.color}/>
+     ))}
 
-ReactDOM.render(React.createElement(Pomodoro, null), document.getElementById('pomodoro-env'));
+      </div>
+    )
+
+  }
+
+  }
+
+  ReactDOM.render(<Pomodoro />, document.getElementById('pomodoro-env'))
