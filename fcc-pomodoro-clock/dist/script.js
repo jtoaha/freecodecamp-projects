df5ae8112c17bee4eed5d2e280eb0276926a0488
Jamila Toaha <jtoahacoding@gmail.com>
2020-08-10 23:46:38 -0400

    added break and session components- starter

--- fcc-pomodoro-clock/dist/script.js
+++ fcc-pomodoro-clock/dist/script.js
@@ -2,9 +2,14 @@
 //Background Items Array will be used to populate BackgroundItems components
 let backgroundItemsArray = [
   {name: 'title-item', text: 'Pomodoro Clock built with React.js', color: '#ffffff'},
-  {name: 'sun-item', text: '', color: 'yellow'},
+  {name: 'sun-item', text: '⬛ - ⬛', color: 'yellow'},
   {name: 'horizontal-item', text: '', color: 'pink'},
   {name: 'timer-case-item', text: '', color: 'red'},
+]
+
+let labelArray = [
+   {name: 'session-label', text: 'Session', duration: 25, increment: 'session-increment', decrement: 'session-decrement'},
+  {name: 'break-label', text: 'Break', duration: 5, increment: 'break-increment', decrement: 'break-decrement'}
 ]
 
 
@@ -18,6 +23,26 @@ const BackgroundItem = props => {
   )
 }
 
+class Label extends React.Component {
+      constructor(props) {
+        super(props)
+        this.state = {
+          duration: this.props.duration
+        }
+      }
+
+      render() {
+
+        return(
+          <div id= {this.props.name} style={{backgroundColor: 'white'}}>
+            <h2>{this.props.text}</h2>
+            <div>
+            ↑ {this.state.duration} ↓
+            </div>
+          </div>
+        )
+      }
+}
 
 
 class Pomodoro extends React.Component {
@@ -35,6 +60,16 @@ class Pomodoro extends React.Component {
           text= {item.text}
           color= {item.color}/>
      ))}
+     {
+       labelArray.map(label => (
+         <Label
+         key = {label.name}
+         name = {label.name}
+         text = {label.text}
+         duration= {label.duration}/>
+       )
+      )
+     }
 
       </div>
     )
